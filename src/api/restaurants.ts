df957b1068a91711849a8f93e6564d38a06db543
KabeerJamal <kabeerjamal7@gmail.com>
2026-04-11 10:42:10 +0200

    feat: implement raw API fetch with centralised error handling

--- src/api/restaurants.ts
+++ src/api/restaurants.ts
@@ -1,6 +1,14 @@
+import { handleError, handleResponse } from "./clientUtils";
+import { API_CONSTANTS } from "./endpoints";
 import { RestaurantAPIResponse } from "../types";
 
 
 export async function fetchRestaurantData(postcode: string): Promise<RestaurantAPIResponse> {
-    return {} as RestaurantAPIResponse
+    try {
+        const response: Response = await fetch(API_CONSTANTS.GET_RESTAURANTS(postcode))
+
+        return handleResponse<RestaurantAPIResponse>(response)
+    } catch (error) {
+        return handleError(error)
+    }
 }
