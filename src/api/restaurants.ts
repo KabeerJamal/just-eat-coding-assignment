70f98363cbecda8da511c31960415bd46a42e875
KabeerJamal <kabeerjamal7@gmail.com>
2026-04-11 09:49:55 +0200

    feat: add fetchRestaurantData skeleton for TDD

---
+++ src/api/restaurants.ts
@@ -0,0 +1,6 @@
+import { RestaurantAPIResponse } from "../types";
+
+
+export async function fetchRestaurantData(postcode: string): Promise<RestaurantAPIResponse> {
+    return {} as RestaurantAPIResponse
+}
