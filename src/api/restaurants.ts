c41e673ded6d3bb81e9b8d6183b1e276bdbb607f
KabeerJamal <kabeerjamal7@gmail.com>
2026-04-11 11:18:21 +0200

    refactor: replace native fetch with fetchWithTimeout

--- src/api/restaurants.ts
+++ src/api/restaurants.ts
@@ -1,11 +1,11 @@
 import { handleError, handleResponse } from "./clientUtils";
 import { API_CONSTANTS } from "./endpoints";
 import { RestaurantAPIResponse } from "../types";
-
+import { fetchWithTimeout } from "./clientUtils";
 
 export async function fetchRestaurantData(postcode: string): Promise<RestaurantAPIResponse> {
     try {
-        const response: Response = await fetch(API_CONSTANTS.GET_RESTAURANTS(postcode))
+        const response: Response = await fetchWithTimeout(API_CONSTANTS.GET_RESTAURANTS(postcode))
 
         return handleResponse<RestaurantAPIResponse>(response)
     } catch (error) {
