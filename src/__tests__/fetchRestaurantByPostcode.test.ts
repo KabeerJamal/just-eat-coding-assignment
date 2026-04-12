3ecab2d4f32e23c53318bb8dc806a57bc170a763
KabeerJamal <kabeerjamal7@gmail.com>
2026-04-12 08:12:37 +0200

    refactor: improve readability for test cases by including describe keyword

--- src/__tests__/fetchRestaurantByPostcode.test.ts
+++ src/__tests__/fetchRestaurantByPostcode.test.ts
@@ -7,27 +7,32 @@ jest.mock("../api/restaurants");
 
 const mockPostcode = "EC4M 7RF";
 
-test("should successfully orchestrate fetching and filtering data", async () => {
-    const mockRawData = {
-        restaurants: [{ name: "Test Resto", rating: { starRating: 5 } }]
-    };
-    (fetchRestaurantData as jest.Mock).mockResolvedValue(mockRawData);
 
-    const result = await fetchRestaurantsByPostcode(mockPostcode);
+describe('fetchRestaurantsByPostcode' , () => {
 
-    // verify the orchestiration
-    expect(fetchRestaurantData).toHaveBeenCalledWith(mockPostcode);
-    expect(result).toHaveLength(1);
-
-    const firstResto = result[0]!;
-
-    expect(firstResto.name).toBe("Test Resto");
-    expect(firstResto.rating).toBe(5);
-});
-
-//Network failure
-test("should propagate API errors upward to the UI layer", async () => {
-    const apiError = new APIError("Rate limit exceeded", 429);
-    (fetchRestaurantData as jest.Mock).mockRejectedValue(apiError);
-    await expect(fetchRestaurantsByPostcode(mockPostcode)).rejects.toThrow(apiError);
-});
+    test("should successfully orchestrate fetching and filtering data", async () => {
+        const mockRawData = {
+            restaurants: [{ name: "Test Resto", rating: { starRating: 5 } }]
+        };
+        (fetchRestaurantData as jest.Mock).mockResolvedValue(mockRawData);
+    
+        const result = await fetchRestaurantsByPostcode(mockPostcode);
+    
+        // verify the orchestiration
+        expect(fetchRestaurantData).toHaveBeenCalledWith(mockPostcode);
+        expect(result).toHaveLength(1);
+    
+        const firstResto = result[0]!;
+    
+        expect(firstResto.name).toBe("Test Resto");
+        expect(firstResto.rating).toBe(5);
+    });
+    
+    //Network failure
+    test("should propagate API errors upward to the UI layer", async () => {
+        const apiError = new APIError("Rate limit exceeded", 429);
+        (fetchRestaurantData as jest.Mock).mockRejectedValue(apiError);
+        await expect(fetchRestaurantsByPostcode(mockPostcode)).rejects.toThrow(apiError);
+    });
+    
+})
