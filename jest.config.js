5a4ccf92b8ebec1d3ea31b019e517dbb34f98086
KabeerJamal <kabeerjamal7@gmail.com>
2026-04-12 20:34:46 +0200

    chore: update Jest configuration to support separate environments for unit and UI tests, and add necessary testing libraries

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,24 @@
-// jest.config.js
+// jest.config.js, split environments: Node for unit tests, jsdom for RTL UI tests
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testPathIgnorePatterns: ["<rootDir>/src/__tests__/helpers/","<rootDir>/dist/"]
-}
+  projects: [
+    {
+      displayName: 'unit',
+      preset: 'ts-jest',
+      testEnvironment: 'node',
+      testMatch: ['<rootDir>/src/__tests__/**/*.test.ts'],
+      testPathIgnorePatterns: [
+        '<rootDir>/node_modules/',
+        '<rootDir>/dist/',
+        '<rootDir>/src/__tests__/helpers/',
+      ],
+    },
+    {
+      displayName: 'ui',
+      preset: 'ts-jest',
+      testEnvironment: 'jsdom',
+      testMatch: ['<rootDir>/src/__tests__/uiTesting/**/*.test.tsx'],
+      setupFilesAfterEnv: ['<rootDir>/src/__tests__/setupTests.ts'],
+      testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+    },
+  ]
+};
